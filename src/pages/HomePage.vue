e2f377d84363a7ba6e146155fa25df013696f242
Tune <wuxixin2000@163.com>
2025-01-22 16:14:59 +0800

    feat(临时暂存): 图片模块

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,68 +1,100 @@
 <template>
-  <header class="welcome-header">
-    <div class="content">
-      <h1 class="title">{{ msg }}</h1>
-      <p class="description">开启智能协作云图库之旅，畅享便捷的图片管理与协作体验。</p>
-    </div>
-  </header>
+  <div class="box">
+    <!-- 图片列表 -->
+    <a-list
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
+      :data-source="dataList"
+      :pagination="pagination"
+      :loading="loading"
+    >
+      <template #renderItem="{ item: picture }">
+        <a-list-item style="padding: 0">
+          <!-- 单张图片 -->
+          <a-card hoverable>
+            <template #cover>
+              <img
+                style="height: 180px; object-fit: cover"
+                :alt="picture.name"
+                :src="picture.url"
+              />
+            </template>
+            <a-card-meta :title="picture.name">
+              <template #description>
+                <a-flex>
+                  <a-tag color="green">
+                    {{ picture.category ?? '默认' }}
+                  </a-tag>
+                  <a-tag v-for="tag in picture.tags" :key="tag">
+                    {{ tag }}
+                  </a-tag>
+                </a-flex>
+              </template>
+            </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
+  </div>
 </template>
 
 <script setup lang="ts">
-const msg = "欢迎使用智能协作云图库平台";
+// 数据
+import { computed, onMounted, reactive, ref } from 'vue'
+import { message } from 'ant-design-vue'
+import { listPictureVoByPageUsingPost } from '@/api/pictureController'
+
+const dataList = ref([])
+const total = ref(0)
+const loading = ref(true)
+
+// 搜索条件
+const searchParams = reactive<API.PictureQueryRequest>({
+  current: 1,
+  pageSize: 12,
+  sortField: 'createTime',
+  sortOrder: 'descend'
+})
+
+// 分页参数
+const pagination = computed(() => {
+  return {
+    current: searchParams.current ?? 1,
+    pageSize: searchParams.pageSize ?? 10,
+    total: total.value,
+    // 切换页号时，会修改搜索参数并获取数据
+    onChange: (page: any, pageSize: any) => {
+      searchParams.current = page
+      searchParams.pageSize = pageSize
+      fetchData()
+    }
+  }
+})
+
+// 获取数据
+const fetchData = async () => {
+  loading.value = true
+  const res = await listPictureVoByPageUsingPost(searchParams)
+  console.log(res.data)
+  if (res.data.data) {
+    dataList.value = res.data.data.records ?? [] as any
+    total.value = res.data.data.total ?? 0
+  } else {
+    message.error('获取数据失败，' + res.data.message)
+  }
+  loading.value = false
+}
+
+// 页面加载时请求一次
+onMounted(() => {
+  fetchData()
+})
+
 </script>
 
 <style scoped>
-.welcome-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height  : 100vh;
-  background-color: #f8f9fa;
-  position: relative;
-  overflow: hidden;
-  font-family: 'Roboto', sans-serif;
-}
-
-.content {
-  text-align: center;
-  color: #333;
-  z-index: 1;
-  animation: fadeInUp 1s ease;
-}
-
-.title {
-  font-size: 48px;
-  font-weight: 300; /* 调整字体粗细 */
-  margin-bottom: 20px;
-  color: #555; /* 调整字体颜色 */
-}
-
-.description {
-  font-size: 20px;
-  font-weight: 300;
-  margin-bottom: 30px;
-  opacity: 0.8;
-  color: #666; /*调整字体颜色 */
-}
-
-@keyframes fadeInUp {
-  from {
-    opacity: 0;
-    transform: translateY(50px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-.welcome-header::before {
-  content: '';
-  position: absolute;
-  top  : 0;
-  left : 0;
-  width : 100%;
-  height : 100%;
-  background-color: rgba(0, 0, 0, 0.05);
+.box {
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 5px;
 }
 </style>
